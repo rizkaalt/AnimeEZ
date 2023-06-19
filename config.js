4da36bd29a2073b0f7fc34f86928ce3227a2d4db
RIZKA    ----- Anime Lovers ----- <116262828+rizkaalt@users.noreply.github.com>
2023-06-19 06:48:41 +0000

    test

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "api_url": "http://localhost:6969",
+  "api_url": "rizka-anime-api.vercel.app:3000",
   // Enter your api host's url or if hosting locally/on your own computer use https://localhost:6969
   
   "time_update": "200",
   // Enter Seconds for updating recently added and popular list. For disabling it Enter disable
   
-  "port": "8000", 
+  "port": "3000", 
     // Enter port for your website
 }
